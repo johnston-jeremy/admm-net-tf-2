4ffcf3e321e060d1d6e555bdc5871fc610728f6a
Jeremy Johnston <jeremyjohnston@jeremys-mbp-2.attlocal.net>
2020-05-11 14:06:23 -0400

    asd

---
+++ problem.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 18:53:53 2020
+
+@author: jeremyjohnston
+"""
+
+import numpy as np
+
+
+class Problem:
+  def __init__(self, size_A, scen, partition=False, **kwargs):
+    M = size_A[0]
+    N = size_A[1]
+
+    if 'N_part' in kwargs.keys():
+      self.N_part = kwargs['N_part']
+
+    self.partition = partition
+    
+    self.scen = scen
+    # ptypes = ['gaussian', 'siso']
+    # dgaussian = dict(admm=0.1,lista=0.1)
+    # dsiso = dict(admm=0.02,lista=0.1)
+    # self.lambda_init = dict(gaussian=dgaussian, siso=dsiso)
+
+    if (scen == 'gaussian'):
+      self.A = np.random.normal(size=(M,N))
+      # normalize cols
+      self.A = np.matmul(self.A,np.diag(1/np.sqrt(np.sum(self.A**2,axis=0))))
+      
+    elif (scen == 'siso'):
+      m = np.arange(M)[:,None]
+      n = np.arange(N)
+      # self.A = np.cos(np.pi*m*n/N)
+      self.A = np.exp(1j*2*np.pi*m*n/N)/np.sqrt(M)
+      # self.A = np.exp(1j*2*np.pi*m*n/N)
+    
+    elif scen == 'siso_d':
+      # N = 8
+      # Ngl = 5 # range
+      # Ngk = 3 # doppler
+
+      N = 20
+      Ngl = 10 # range
+      Ngk = 4 # doppler
+
+      self.A = np.zeros((N,Ngk*Ngl),dtype=np.complex128)
+      for l in range(Ngl):
+        for k in range(Ngk):
+          # self.A[:, l*Ngk + k] = np.cos(np.pi*((l/Ngl)*np.arange(N)+(k/Ngk)*np.arange(N)**2))
+          self.A[:, l*Ngk + k] = np.exp(1j*np.pi*((l/Ngl)*np.arange(N)+(k/Ngk)*np.arange(N)**2))/np.sqrt(N)
+
+    elif (scen == 'mimo_real'):
+      Np = 3
+      Ntx = 2
+      Nrx = 2
+
+      Ngl = 5 # range grid
+      Ngk = 5 # DoA grid
+
+      ngl = np.arange(Ngl)
+      ngk = np.arange(Ngk)
+
+      v = np.cos(np.pi*np.outer(np.arange(Np),ngl)/Ngl)
+      hrx = np.cos(np.pi*np.outer(np.arange(Nrx),ngk)/Ngk)
+      htx = np.cos(np.pi*np.outer(np.arange(Ntx),ngk)/Ngk)
+      h = khatri_rao(hrx,htx)
+      self.A = np.kron(h,v)
+
+    elif (scen == 'mimo_r'):
+      # Np = 2
+      # Ntx = 1
+      # Nrx = 2
+
+      # Ngl = 3 # range grid
+      # Ngk = 3 # DoA grid
+
+      Np = 4;
+      Ntx = 2;
+      Nrx = 1;
+      Ngl = 4;
+      Ngk = 4;
+
+      ngl = np.arange(Ngl)
+      ngk = np.arange(Ngk)
+      nn = np.arange(Np)
+      nrx = np.arange(Nrx)
+      ntx = np.arange(Ntx)
+
+      v = np.exp(1j*2*np.pi*np.outer(nn,ngl)/Ngl)
+      hrx = np.exp(1j*2*np.pi*np.outer(nrx,ngk)/Ngk)
+      htx = np.exp(1j*2*np.pi*np.outer(ntx,ngk)/Ngk)
+      h = khatri_rao(hrx,htx)
+      A = np.kron(h,v)
+      self.A=A
+      # Atop = np.concatenate((np.real(A),-np.imag(A)),axis=1)
+      # Abot = np.concatenate((np.imag(A),np.real(A)),axis=1)
+      # self.A = np.concatenate((Atop,Abot),axis=0)
+
+    elif (scen == 'mimo'):
+      # Np = 2
+      # Ntx = 1
+      # Nrx = 2
+
+      # Ngl = 3 # range grid
+      # Ngk = 3 # DoA grid
+
+      Ntx = 4;
+      Nrx = 2;
+
+      Ngk = 16;
+
+
+      ngk = np.arange(Ngk)
+      nrx = np.arange(Nrx)
+      ntx = np.arange(Ntx)
+
+      hrx = np.exp(1j*2*np.pi*np.outer(nrx,ngk)/Ngk)
+      htx = np.exp(1j*2*np.pi*np.outer(ntx,ngk)/Ngk)
+      self.A=khatri_rao(hrx,htx)
+      # Atop = np.concatenate((np.real(A),-np.imag(A)),axis=1)
+      # Abot = np.concatenate((np.imag(A),np.real(A)),axis=1)
+      # self.A = np.concatenate((Atop,Abot),axis=0)
+      #   
+
+    elif (scen == 'mimo_d'):
+      
+      # N = 4;
+      # Ntx = 2;
+      # Nrx = 1;
+      # # sampling grid
+      # M = N*Ntx*Nrx;
+      # Ngm = 2; 
+      # Ngl = 3;
+      # Ngk = 3;
+
+      # N = 2;
+      # Ntx = 2;
+      # Nrx = 2;
+      # # sampling grid
+      # M = N*Ntx*Nrx;
+      # Ngm = 2; 
+      # Ngl = 2;
+      # Ngk = 4;
+
+      N = 5;
+      Ntx = 2;
+      Nrx = 2;
+      # sampling grid
+      M = N*Ntx*Nrx;
+      Ngm = 2; # doppler 
+      Ngl = 5; # range
+      Ngk = 4; # DoA
+
+      nn = np.arange(N)
+      nrx = np.arange(Nrx)
+      ntx = np.arange(Ntx)
+
+      self.A = np.zeros((M,Ngm*Ngl*Ngk),dtype=np.complex128);
+      for l in range(Ngl):
+          for k in range(Ngk):
+              for m in range(Ngm):
+                  v = np.exp(1j*np.pi*((l/Ngl)*nn.T + (m/Ngm)*nn.T**2));
+                  H = np.outer(np.exp(1j*2*np.pi*(k/Ngk)*nrx),np.exp(1j*2*np.pi*(k/Ngk)*ntx))
+                  h = H.ravel();
+                  self.A[:, l*Ngm*Ngk + k*Ngm + m] = np.kron(h,v)/np.sqrt(N*Ntx*Nrx);
+    
+    elif scen == 'yin':
+      # dimensions of the sparse signal x
+      n = 8
+      # dimension of the compressed signal y
+      m = 4
+      # sparsity : non-zero values over n
+      sparsity = 2
+
+      # training and testing number:
+      train_number = 50000
+      valid_number = 100
+      # number of training iterations (epochs)
+      numIter = 500
+
+      # w state of randomness
+      # rng = np.random.RandomState(23)
+
+
+      # create the 1:N sample
+      sample = np.array(list(range(m))).reshape(m, 1)
+      # create the frequency grid
+      f_grid = 1/n*np.array(list(range(n))).reshape(1, n)
+
+      # generate the dictionary, without the sampling matrix Phi
+      C = np.exp(1j*2*np.pi*np.matmul(sample, f_grid))
+      A = C.copy()
+
+      # generate the dictionary with the sampling matrix Phi
+      C = np.exp(1j*2*np.pi*np.matmul(sample, f_grid))
+      phi = np.ones([m])
+      p = 0.8 # sparsity level
+      Ns = int(np.floor((1-p)*m)) # the sampling number, note that should be int
+      index = rd.sample(range(m),Ns)
+      phi[index] = 0
+      A = C.copy()
+      for i in range(n):
+        A[index, i] = 0
+        # print(A[index, i])    
+      self.A = A
+
+    # normalize columns
+    # self.A = np.matmul(self.A,np.diag(1/np.sqrt(np.sum(self.A**2,axis=0))))
+
+    if self.partition == True:
+      self.A = np.concatenate((self.A, np.eye(np.shape(self.A)[0])),axis=1)
+
+    
+
+    return
+
+  
+    
+  def size(self, dim=None):
+    if dim is None:
+      return self.A.shape
+    else:
+      return self.A.shape[dim]
+
+def khatri_rao(a, b):
+    c = a[...,:,np.newaxis,:] * b[...,np.newaxis,:,:]
+    # collapse the first two axes
+    return c.reshape((-1,) + c.shape[2:])
+
+#L = chol( speye(m) + 1/rho*(A*A'), 'lower' );
+# class GaussianProblem(Problem):
---
+++ train_net.py
@@ -0,0 +1,149 @@
+import problem
+import admm2 as admm
+import tensorflow as tf
+import numpy as np
+import scipy as sp
+import scipy.io
+import time
+import random as rd
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+def gen_net(properties, *args, **kwargs):
+  f = properties['net_type']
+  n = properties['num_stages']
+  p = properties['problem']
+  
+  if f == 'lista':
+    a = lista.ISTANet(p, n, *args, **kwargs)
+  elif f == 'admm':
+    a = admm.ADMMNet(p, n, *args, **kwargs)
+  elif f == 'admm_i':
+    a = admm_i.ADMMNet(p, n, *args, **kwargs)
+
+  return a
+
+def train_net(a, data, props, **kwargs):
+
+  loss = props['loss']
+  optim = props['optimizer']
+  rate = props['learning_rate']
+
+  if props['schedule'] == True:
+    STEPS_PER_EPOCH = Ntrain//batch_size
+    lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
+      rate,
+      decay_steps=STEPS_PER_EPOCH*10,
+      decay_rate= 1,
+      staircase=True)
+    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+    #   adamrate,
+    #   decay_steps=STEPS_PER_EPOCH*100,
+    #   decay_rate=.5,
+    #   staircase=True)  
+    # lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+    #   boundaries=[500], values=list(rate*np.array([1e0,1e-1])), name=None
+    # )
+    # breakpoint()
+    import matplotlib.pyplot as plt
+    step = np.linspace(0,10000)
+    lr = lr_schedule(step)
+    plt.figure(figsize = (8,6))
+    plt.plot(step/STEPS_PER_EPOCH, lr)
+    plt.ylim([0,max(plt.ylim())])
+    plt.xlabel('Epoch')
+    _ = plt.ylabel('Learning Rate')
+    plt.show()
+
+    optimizer = tf.keras.optimizers.Adam(lr_schedule)
+  else:
+    if optim == 'adam':
+      optimizer = tf.keras.optimizers.Adam(learning_rate=rate)
+    elif optim == 'sgd':
+      optimizer = tf.keras.optimizers.SGD(learning_rate=rate, momentum=0.9)
+      
+  if loss == 'MSE':
+    a.compile(optimizer, loss=tf.keras.losses.MeanSquaredError())
+  elif loss == 'NMSE':
+    a.compile(optimizer, loss=admm.MeanPercentageSquaredError())
+  elif loss == 'SDR':
+    a.compile(optimizer, loss=admm.SDR())
+  elif loss == 'MSLE':
+    a.compile(optimizer, loss=tf.keras.losses.MeanSquaredLogarithmicError())
+  elif loss == 'MAE':
+    a.compile(optimizer, loss=tf.keras.losses.MeanAbsoluteError())
+  elif loss == 'MAPE':
+    a.compile(optimizer, loss=tf.keras.losses.MeanAbsolutePercentageError())
+    
+  a.fit(data[1], 
+        data[0], 
+        epochs=props['epochs'], 
+        batch_size = props['batch_size']
+  )
+  
+  return a
+
+def save_net(net, properties):
+  f = properties['net_type']
+  n = properties['num_stages']
+  p = properties['problem']
+  s1 = properties['s1']
+  s2 = properties['s2']
+  snr = properties['SNR']
+
+  filename = p.scen + '_{0}x{1}'.format(p.size(0),p.size(1)) + '_' + 'SNR=' + str(snr) + 'dB_' + str(n) + 'stages_' + 's1=' + str(s1) + '_s2=' + str(s2) + '_' + f + '_' + net.tied + '_' + time.strftime("%m_%d_%Y_%H_%M_%S",time.localtime())
+  filepath = './nets/' + filename
+  os.mkdir(filepath)
+  net.save_weights(filepath+'/weights')
+  np.save(filepath+'/A', p.A)
+
+  return filename
+
+def test_net(a, N=1e3):
+  x,y = gen_samples(p.A,N)
+  xhat = a.predict_on_batch(y)
+  print(10*np.log10(np.mean((np.sum((x-xhat)**2,axis=1)/np.sum(x**2,axis=1)))))
+  return
+
+def loadnet():
+  p = problem.Problem((20,40), 'mimo')
+
+
+  # x = np.load('./trainingdata/1e5_mimo_c_4x8_x.npy')
+  # y = np.load('./trainingdata/1e5_mimo_c_4x8_y.npy')
+  
+  x = np.load('./trainingdata/1e6_mimo_20x40_x.npy')
+  y = np.load('./trainingdata/1e6_mimo_20x40_y.npy')
+
+  props = dict(net_type='admm', 
+              num_stages=4, 
+              problem=p
+          )
+          
+  a = load_net.get_net(4, 'untied', params_init=params_init)
+
+  optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+  # optimizer = tf.keras.optimizers.SGD(learning_rate=1e-2, momentum=0.9, nesterov=False)
+
+  # a.compile(optimizer, loss=tf.keras.losses.MeanSquaredError())
+  a.compile(optimizer, loss=lista.MeanPercentageSquaredError())
+
+  a = train_net(a, p, (x,y), props, epochs=100)
+
+  save_net(a,props)
+
+  # x = np.load('./trainingdata/1e5_mimo_20x40_x.npy')
+  # y = np.load('./trainingdata/1e5_mimo_20x40_y.npy')
+
+  xhat = a.predict_on_batch(y)
+  breakpoint()
+  print(10*np.log10(np.mean(np.sum((x-xhat)**2,axis=1)/np.sum(x**2,axis=1))))
+
+def print_vars(a):
+  printvars = ['lambda:0','alpha:0']
+  for l in a.Layers:
+    for v in l.variables:
+      if v.name in printvars:
+        print(v.name + ' =', v.numpy())
